c7f8edab44473eb3116cb1e02ad907e4ee0a18f9
Marcel Indlekofer <marcel.indlekofer@uni-konstanz.de>
2023-01-25 14:14:12 +0100

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'sherlook.app.v1',
+  appName: 'sherlook-app-v1',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
